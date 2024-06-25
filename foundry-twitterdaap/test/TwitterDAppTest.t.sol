eaea2669002407f03ffaf30206eed17fd843a9cb
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-06-25 10:51:19 -0300

    feat: added contracts

---
+++ foundry-twitterdaap/test/TwitterDAppTest.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {Test, console} from 'forge-std/Test.sol';
+import {TwitterDApp} from '../src/TwitterDApp.sol';
+
+contract TwitterDAppTest is Test {
+  TwitterDApp public counter;
+
+  function setUp() external {
+    twitterdapp = new TwitterDApp();
+  }
+}
