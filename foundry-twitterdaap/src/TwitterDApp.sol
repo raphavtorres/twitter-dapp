2775dc51cca25cc3045f12b78f51a6f472a89cac
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-06-25 09:56:17 -0300

    init: project setup

---
+++ foundry-twitterdaap/src/TwitterDApp.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
+import '@openzeppelin/contracts/utils/Base64.sol';
+
+contract TwitterDapp is ERC721('TwitterDApp', 'TDAPP') {
+  uint256 tokenId;
+  Tweet[] public tweets;
+
+  struct Tweet {
+    string name;
+    string description;
+    uint256 upvotes;
+    string[] comments;
+    address fromAddress;
+  }
+
+  function tokenURI(
+    uint256 _tokenId
+  ) public view override returns (string memory) {
+    Tweet memory tweet = tweets[_tokenId];
+
+    bytes memory dataURI = abi.encodePacked(
+      '{',
+      '"name":',
+      '"',
+      tweet.name,
+      '",'
+      '"description":',
+      '"',
+      tweet.description,
+      '"',
+      ',',
+      '"attributes":',
+      '[',
+      '{',
+      '"trait_type":',
+      '"Upvotes",',
+      '"value":',
+      Strings.toString(tweet.upvotes),
+      '}',
+      ']',
+      '}'
+    );
+
+    return
+      string(
+        abi.encodePacked(
+          'data:application/json;base64,',
+          Base64.encode(dataURI)
+        )
+      );
+  }
+
+  function writeTweet(string memory prefName, string memory prefDesc) public {
+    _safeMint(msg.sender, tokenId);
+    tweets.push(
+      Tweet({
+        name: prefName,
+        description: prefDesc,
+        upvotes: 0,
+        comments: new string[](0),
+        fromAddress: msg.sender
+      })
+    );
+    tokenId++;
+  }
+
+  function upvote(uint tweetIndex) public {
+    tweets[tweetIndex].upvotes++;
+  }
+
+  function addComment(uint256 tweetIndex, string memory prefComments) public {
+    tweets[tweetIndex].comments.push(prefComments);
+  }
+
+  function getAllTweets() public view returns (Tweet[] memory) {
+    return tweets;
+  }
+}
---
+++ frontend/constants.ts
@@ -0,0 +1,19 @@
+/**
+ * Alchemy API key
+ * @constant
+ */
+export const ALCHEMY_KEY = "DM2on7cd9gOZNeqCe8oxeLOQ_5LqBDZC";
+
+export const CONTRACT_ADDRESS = "<deployed_contract_address>";
+
+export const OPENSEA_ASSETS_URL = "https://testnets.opensea.io/assets/mumbai";
+export const CLIENT_ID = "<web3auth_client_id>";
+export const RPC_TARGET = "<Quick_Node_Polygon_HTTP_Provider>";
+export const APP_LOGO = "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg";
+export const ADAPTER_TWITTER_CLIENT_VERIFIER =
+	"<web3auth_custom_auth_verifier_name>";
+export const ADAPTER_TWITTER_CLIENT_ID = "<auth0_client_id>";
+export const AUTH0_DOMAIN = "<auth0_domain>";
+export const BIRD_IMAGE =
+	"https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png";
+export const REACT_APP_REFRESH_TIMER = 30;
