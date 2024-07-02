09eaaac029d770e703f6deb142ed3a37c306dfbf
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-07-02 07:45:38 -0300

    refactor: fix and deleted unused code

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,39 +1,39 @@
 <template>
 	<div>
 		<NuxtLayout>
+			<div>
+				<h1 className="title">Web3 Twitter</h1>
+				<p className="title">
+					Contract Address: <br />
+					{{ CONTRACT_ADDRESS }}
+				</p>
+				<!-- <div className="grid">{provider ? loggedInView : unloggedInView}</div> -->
+				<component :is="provider ? 'LoggedInView' : 'UnloggedInView'" />
+			</div>
+			<!-- LOGGED VIEW -->
+			<!-- UNLOGGED VIEW -->
 			<NuxtPage />
 		</NuxtLayout>
 	</div>
 </template>
 <script lang="ts">
-import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { ref, onMounted } from "vue";
+import { CONTRACT_ADDRESS } from "./constants";
 
-const clientId = process.env.WEB3AUTH_CLIENT_ID;
+// import LoggedInView from '@/components/LoggedInView.vue';
+// import UnloggedInView from '@/components/UnloggedInView.vue';
 
-const openloginAdapter = new OpenloginAdapter({
-	adapterSettings: {
-		clientId,
-		network: "testnet",
-		uxMode: "popup",
-		whiteLabel: {
-			name: "Twitter DApp",
-			// logoLight: "<hosted-logo-image-link>",
-			// logoDark: "<hosted-logo-image-link>",
-			defaultLanguage: "en",
-			dark: true, // whether to enable dark mode. defaultValue: false
-		},
-		loginConfig: {
-			// Add login configs corresponding to the providers on modal
-			// Twitter login
-			jwt: {
-				name: "Custom Auth Login",
-				Verifier: "twitter-dap-verifier",
-				typeOfLogin: "twitter",
-				clientId: process.env.AUTH0_CLIENT_ID,
-			},
-		},
-	},
+const web3auth = ref(null);
+const provider = ref(true);
+const tweets = ref(null);
+const comment = ref("");
+const newTweetName = ref("");
+const newTweetDescription = ref("");
+const torusPlugin = ref(null);
+
+provider.value = false; // testing provider
+
+onMounted(() => {
+	// Lógica de inicialização do Web3Auth e outras funcionalidades
 });
-
-web3auth.configureAdapter(openloginAdapter);
 </script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,2 @@
 <template>Hello from pages!</template>
-<script lang="ts">
-import { Web3Auth } from "@web3auth/modal";
-import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
-import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-
-const chainConfig = {
-	// chain identifier for sepolia testnet
-	chainId: process.env.SEPOLIA_CHAIN_ID,
-	// enables comunication with contract deployed on chain hosted on alchemy
-	rpcTarget: process.env.SEPOLIA_RPC_TARGET,
-	// specifies that the chain is EVM compatible
-	chainNamespace: CHAIN_NAMESPACES.EIP155,
-};
-
-const web3auth = new Web3AuthCore({
-	chainConfig: chainConfig,
-});
-
-web3auth.init();
-</script>
+<script lang="ts"></script>
